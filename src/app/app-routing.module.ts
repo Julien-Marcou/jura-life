03bd6b1825de4d660637d5abf17f2fcda450d058
Julien Marcou <julien.marcou.dev@gmail.com>
2021-12-03 07:32:04 +0100

    Fix routing error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Router, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
 
@@ -7,4 +7,12 @@ const routes: Routes = [];
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+
+  constructor(private readonly router: Router) {
+    this.router.errorHandler = (): void => {
+      this.router.navigate(['']);
+    };
+  }
+
+}
