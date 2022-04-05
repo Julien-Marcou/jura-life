cc61d341b7e3731e8bb678ed86b5f718829d3864
Julien Marcou <julien.marcou.dev@gmail.com>
2022-04-05 22:17:19 +0200

    Add <noscript> tag for map SEO & accessibility

---
+++ src/index-noscript.ts
@@ -0,0 +1,75 @@
+import { TargetOptions } from '@angular-builders/custom-webpack';
+import { JURA_POINTS_OF_INTEREST } from './app/constants/jura-points-of-interest.constants';
+import { PINS } from './app/constants/pins.constants';
+import { SerializedPointOfInterest } from './app/models/serialized-point-of-interest';
+import { SerializedTrail } from './app/models/serialized-trail';
+import { TrailMetadataService } from './app/services/trail-metadata.service';
+import { TrailParserNodeJsService } from './app/services/trail-parser-nodejs.service';
+
+const html = (strings: TemplateStringsArray, ...values: Array<unknown>): string => {
+  let result = '';
+  for (let i = 0; i < values.length; i++) {
+    result += strings[i];
+    const value = values[i];
+    if (value instanceof Array) {
+      result += value.join('');
+    }
+    else {
+      result += value;
+    }
+  }
+  result += strings[values.length];
+  return result;
+};
+
+const renderTrailContent = async (serializedTrail: SerializedTrail): Promise<string> => {
+  const parsedTrail = await TrailParserNodeJsService.parseTrail(serializedTrail);
+  const trailMetadata = TrailMetadataService.getTrailMetadata(parsedTrail);
+  return html`
+    <li>
+      <h4>${serializedTrail.startingPoint}</h3>
+      <p>
+        Longueur : ${Math.round(trailMetadata.length * 100) / 100}km<br>
+        Temps de marche : ${serializedTrail.duration}<br>
+        Altitude min : ${Math.round(trailMetadata.minElevation)}m<br>
+        Altitude max : ${Math.round(trailMetadata.maxElevation)}m<br>
+        Altitude de départ : ${Math.round(trailMetadata.startingElevation)}m<br>
+        Altitude d'arrivée : ${Math.round(trailMetadata.endingElevation)}m<br>
+        Dénivelé positif : ${Math.round(trailMetadata.positiveElevation)}m<br>
+        Dénivelé négatif : ${Math.round(trailMetadata.negativeElevation)}m
+      </p>
+    </li>
+  `;
+};
+
+const renderPoiContent = async (id: string, poi: SerializedPointOfInterest): Promise<string> => html`
+  <article>
+    <h2 id="${id}">${poi.name}</h2>
+    <p>
+      Catégorie : ${PINS[poi.type].label}<br>
+      ${poi.description ? html`Description : ${poi.description}` : ''}
+    </p>
+    <h3>Coordonées GPS</h3>
+    <p>
+      Latitude : ${poi.latitude}<br>
+      Longitude : ${poi.longitude}
+    </p>
+    ${poi.trails ? html`
+      <h3>Accès</h3>
+      <ul>
+        ${await Promise.all(poi.trails.map((serializedTrail) => renderTrailContent(serializedTrail)))}
+      </ul>
+    ` : ''}
+  </article>
+`;
+
+const renderNoscriptContent = async (): Promise<string> => html`
+  <h1>Carte Intéractive des Points d'Intérêt Touristique du Haut-Jura</h1>
+  ${await Promise.all(Object.entries(JURA_POINTS_OF_INTEREST).map(([id, poi]) => renderPoiContent(id, poi)))}
+`;
+
+export default async (_: TargetOptions, indexHtml: string): Promise<string> => {
+  const noscriptContentTag = '{{NOSCRIPT_CONTENT}}';
+  const noscriptContent = await renderNoscriptContent();
+  return indexHtml.replace(noscriptContentTag, noscriptContent);
+};
