1b094cb2daa2ba61212450fddeacacb16790e921
Julien Marcou <julien.marcou.dev@gmail.com>
2022-06-16 18:12:16 +0200

    Improve zone.js flags

---
+++ src/zone-flags.ts
@@ -0,0 +1,66 @@
+/* eslint-disable no-underscore-dangle */
+(window as ZoneGlobalConfigurations).__Zone_disable_customElements = true;
+(window as ZoneGlobalConfigurations).__Zone_disable_canvas = true;
+(window as ZoneGlobalConfigurations).__Zone_disable_geolocation = true;
+(window as ZoneGlobalConfigurations).__Zone_disable_blocking = true;
+(window as ZoneGlobalConfigurations).__Zone_disable_MutationObserver = true;
+(window as ZoneGlobalConfigurations).__Zone_disable_FileReader = true;
+
+const disabledEvents: Array<keyof HTMLElementEventMap | keyof WindowEventHandlersEventMap | keyof WebSocketEventMap | keyof XMLHttpRequestEventTargetEventMap> = [
+  // Scroll
+  'scroll',
+  'wheel',
+
+  // Mouse
+  'dblclick',
+  'mousedown',
+  'mouseenter',
+  'mouseleave',
+  'mousemove',
+  'mouseout',
+  'mouseover',
+  'mouseup',
+
+  // Touch
+  'touchstart',
+  'touchend',
+  'touchmove',
+  'touchcancel',
+
+  // Pointer
+  'pointerover',
+  'pointerenter',
+  'pointerdown',
+  'pointermove',
+  'pointerup',
+  'pointercancel',
+  'pointerout',
+  'pointerleave',
+  'gotpointercapture',
+  'lostpointercapture',
+
+  // Drag
+  'drag',
+  'dragend',
+  'dragenter',
+  'dragleave',
+  'dragover',
+  'dragstart',
+  'drop',
+
+  // Animation
+  'animationcancel',
+  'animationend',
+  'animationiteration',
+  'animationstart',
+
+  // Window
+  'beforeunload',
+  'unload',
+
+  // Other
+  'resize',
+  'contextmenu',
+];
+(window as ZoneGlobalConfigurations).__zone_symbol__PASSIVE_EVENTS = disabledEvents;
+(window as ZoneGlobalConfigurations).__zone_symbol__UNPATCHED_EVENTS = disabledEvents;
